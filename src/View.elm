d8bb448756c67e55994087e8ce29fee76223543e
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2022-09-29 15:21:52 +0200

    First commit

---
+++ src/View.elm
@@ -0,0 +1,23 @@
+module View exposing (View, map, placeholder)
+
+import Element exposing (Element)
+
+
+type alias View msg =
+    { title : String
+    , body : List (Element msg)
+    }
+
+
+map : (msg1 -> msg2) -> View msg1 -> View msg2
+map fn doc =
+    { title = doc.title
+    , body = List.map (Element.map fn) doc.body
+    }
+
+
+placeholder : String -> View msg
+placeholder moduleName =
+    { title = "Placeholder - " ++ moduleName
+    , body = [ Element.text moduleName ]
+    }
